d0e8bec82ec3c9f7174948319bf4a43ed27acc43
qian.wu.2 <qian.wu.2>
2023-12-12 15:22:40 +0800

    a

--- excelsplit-出勤.py
+++ excelsplit-出勤.py
@@ -2,10 +2,13 @@
 
 import pandas as pd
 
+# 定义月份变量
+month = '2023-11'
+
 # 读取Excel文件
-df = pd.read_excel(r'C:\Users\Arsser\Desktop\外包费用\出勤和报销\外包出勤数据确认-2023.10.xlsx')
+df = pd.read_excel(fr'\\10.12.21.65\share\外包费用\新流程\{month}\Step1-考勤数据\外包钉钉考勤.xlsx')
 
 # 拆分数据
 for value in df['公司'].unique():
     df_subset = df[df['公司'] == value]
-    df_subset.to_excel(rf"C:\Users\Arsser\Desktop\外包费用\出勤和报销\分拆\{value}_10月出勤.xlsx", index=False)
+    df_subset.to_excel(rf"\\10.12.21.65\share\外包费用\新流程\{month}\Step1-考勤数据\chai_{value}_{month}月出勤.xlsx", index=False)
--- excelsplit-报销.py
+++ excelsplit-报销.py
@@ -2,10 +2,12 @@
 
 import pandas as pd
 
+month = '2023-11'
+
 # 读取Excel文件
-df = pd.read_excel(r'C:\Users\Arsser\Desktop\外包费用\出勤和报销\外包报销单据确认-赵思逸1699875477458.xlsx')
+df = pd.read_excel(fr'\\10.12.21.65\share\外包费用\新流程\{month}\Step1-考勤数据\外包钉钉报销.xlsx')
 
 # 拆分数据
 for value in df['公司'].unique():
     df_subset = df[df['公司'] == value]
-    df_subset.to_excel(rf"C:\Users\Arsser\Desktop\外包费用\出勤和报销\分拆\{value}_10月报销.xlsx", index=False)
+    df_subset.to_excel(rf"C:\Users\Arsser\Desktop\外包费用\出勤和报销\chai_{value}_10月报销.xlsx", index=False)
